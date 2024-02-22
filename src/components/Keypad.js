d386e06e81e6f269bca5bffb6ded6730845b10ad
Moises Diaz <Moises.diaz9108@gmail.com>
2024-02-22 12:01:56 -0500

    lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function onChangeHandle(){
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={onChangeHandle} />
+        </div>
     )
 }
 
